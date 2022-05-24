9cc429abb67a7f721669916ef3028c05c6a750db
Sri Shatagopam <76266488+srishatagopam@users.noreply.github.com>
2022-05-23 18:19:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
+    name='LIME-package',
     url='https://https://github.com/srishatagopam/LIME-package',
     author='Wei Ran',
     author_email='aeinrw@gmail.com',
     packages=['LIME-package'],
-    install_requires=[numpy],
+    install_requires=[],
     version='0.1',
     license='MIT',
     description='Wrapper for LIME python implementation.',
