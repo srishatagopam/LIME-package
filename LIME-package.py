bf042b3e7b09880379ae28c140bd88966ac5c1f5
Sri Shatagopam <76266488+srishatagopam@users.noreply.github.com>
2022-05-23 17:56:52 -0700

    Add files via upload

---
+++ LIME-package.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='LIME-package',
+    version='0.0.1',
+    author='Wei Ran',
+    author_email='aeinrw@gmail.com',
+    description='Package wrapper for LIME Python implementation.',
+    long_description_content_type="text/markdown",
+    url='https://github.com/srishatagopam/LIME-package',
+    license='MIT',
+    packages=['LIME-package'],
+)
